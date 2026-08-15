f499c4c16c1d80abbacacec351315481f141d229
HamzaBenMohamd <hamza.study.13@gmail.com>
2026-08-15 14:44:42 +0100

    change variable name

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -3,12 +3,10 @@
 #include <cstddef>
 
 template <typename T, typename F>
-void iter(T *arr_add,const size_t length, F func)
+void iter(T *arr,const size_t length, F func)
 {
-    if (!arr_add || !func)
+    if (!arr || !func)
         return;
     for (size_t i = 0; i < length; i++)
-    {
-        func(arr_add[i]);
-    }
+        func(arr[i]);
 }
