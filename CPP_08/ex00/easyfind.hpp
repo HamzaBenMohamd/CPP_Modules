f2f3563757cb3094c30042fdfbe106d8f770c795
Hamza Ben Mohamd <hamza.study.13@gmail.com>
2026-08-29 11:28:36 +0100

    setup easyfind function (const + non-const) versions

---
+++ CPP_08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include "NotFoundException.hpp"
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+        throw NotFoundException();
+    return it;
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value)
+{
+    typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+        throw NotFoundException();
+    return it;
+}
+
+#endif
