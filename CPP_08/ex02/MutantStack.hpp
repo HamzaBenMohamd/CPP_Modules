b4c637d0645b772cadb3995500d7d66848e1f357
Hamza Ben Mohamd <hamza.study.13@gmail.com>
2026-08-30 16:10:48 +0100

    MutanStack is done

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -1,25 +1,15 @@
 #ifndef MUTANTSTACK_HPP
-#define MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
-#include <stack>
+# include <stack>
 
-// MutantStack is std::stack that became iterable.
-// std::stack is only a container adapter: it hides the underlying container
-// behind its public interface, so there is no way to iterate over it.
-// The underlying container is stored in a *protected* member named "c", so a
-// derived class may access it directly. Therefore we simply inherit from
-// std::stack<T> and re-expose the iterators of that protected container.
 template <typename T>
 class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack() : std::stack<T>()
-        {
-        }
+        MutantStack() : std::stack<T>() {}
 
-        MutantStack(const MutantStack &other) : std::stack<T>(other)
-        {
-        }
+        MutantStack(const MutantStack &other) : std::stack<T>(other) {}
 
         MutantStack &operator=(const MutantStack &other)
         {
@@ -28,31 +18,52 @@ class MutantStack : public std::stack<T>
             return *this;
         }
 
-        ~MutantStack()
+        ~MutantStack() {}
+
+        typedef typename std::stack<T>::container_type::iterator                iterator;
+        typedef typename std::stack<T>::container_type::const_iterator          const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator        reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator  const_reverse_iterator;
+
+        iterator begin()
         {
+            return this->c.begin();
         }
 
-        // All the iterator categories of the underlying container
-        // (std::deque by default). We need "typename" because these are
-        // nested types of a template-dependent base class.
-        typedef typename std::stack<T>::container_type::iterator iterator;
-        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        iterator end()
+        {
+            return this->c.end();
+        }
 
-        // Iteration in storage order: begin() -> first pushed, end() -> past the top.
-        iterator begin() { return this->c.begin(); }
-        iterator end() { return this->c.end(); }
+        const_iterator begin() const
+        {
+            return this->c.begin();
+        }
 
-        const_iterator begin() const { return this->c.begin(); }
-        const_iterator end() const { return this->c.end(); }
+        const_iterator end() const
+        {
+            return this->c.end();
+        }
 
-        // Reverse order: begin from the top (last pushed) down to the bottom.
-        reverse_iterator rbegin() { return this->c.rbegin(); }
-        reverse_iterator rend() { return this->c.rend(); }
+        reverse_iterator rbegin()
+        {
+            return this->c.rbegin();
+        }
 
-        const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-        const_reverse_iterator rend() const { return this->c.rend(); }
+        reverse_iterator rend()
+        {
+            return this->c.rend();
+        }
+
+        const_reverse_iterator rbegin() const
+        {
+            return this->c.rbegin();
+        }
+
+        const_reverse_iterator rend() const
+        {
+            return this->c.rend();
+        }
 };
 
 #endif
