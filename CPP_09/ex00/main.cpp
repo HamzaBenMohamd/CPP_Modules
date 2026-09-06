ff83f2bb53ab4a352977090c5449563215cfff71
Hamza Ben Mohamd <hamza.study.13@gmail.com>
2026-09-06 14:40:41 +0100

    ex00/main.cpp is done

--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -1,28 +1,23 @@
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        std::cout << "Error: could not open file." << std::endl;
+        std::cerr << "Error: could not open file.\n";
         return 1;
     }
 
-    BitcoinExchange btc;
-    try
+    try 
     {
-        btc.loadDatabase("data.csv");
-    }
-    catch (const std::exception& e)
+        BitcoinExchange btc;
+        btc.processInputFile(argv[1]);
+    } 
+    catch (const std::exception& e) 
     {
-        std::cout << e.what() << std::endl;
+        std::cerr << e.what() << '\n';
         return 1;
     }
 
-    // 10 significant digits: precise enough to clearly compare with the
-    // database while still trimming trailing zeros (e.g. "0.9").
-    std::cout << std::setprecision(10);
-
-    btc.processFile(argv[1]);
     return 0;
 }
